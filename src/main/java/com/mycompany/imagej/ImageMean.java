55767767be3671489ef31bf2d49b1a24947b3b12
felix <nibelungen@gmx.ch>
2017-06-19 16:13:19 +0200

    cosmetics

--- src/main/java/com/mycompany/imagej/ImageMean.java
+++ src/main/java/com/mycompany/imagej/ImageMean.java
@@ -8,8 +8,6 @@ import net.imagej.table.DefaultFloatTable;
 import net.imagej.table.Table;
 import net.imglib2.Cursor;
 import net.imglib2.img.Img;
-import net.imglib2.type.NativeType;
-import net.imglib2.type.numeric.NumericType;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
@@ -23,24 +21,19 @@ public class ImageMean<T extends RealType<T>> implements Command {
     @Parameter(type = ItemIO.INPUT, persistKey = "asdf")
     Img<T> img;
 
-
     @Parameter(type = ItemIO.OUTPUT)
     Table res;
 
 
     @Override
     public void run() {
-
-//        Img img = das.getImgPlus().getImg();
-
-
         // Get the number of pixels
         Long numPix = 1l;
         for (int d= 0; d < img.numDimensions(); d ++) {
             numPix *= img.dimension(d);
         }
 
-
+        // Sum up all the pixels
         Cursor<T> cur = img.cursor();
         T sum = img.firstElement().copy();
         cur.next();
@@ -49,33 +42,26 @@ public class ImageMean<T extends RealType<T>> implements Command {
             sum.add(cur.get());
         }
 
+        // Compute the mean pixel value and display it in a table
         Float mean = sum.getRealFloat() / numPix.floatValue();
         res = new DefaultFloatTable(1,1);
         res.setColumnHeader(0, "Image Mean Pixel Value");
         res.set(0, 0, mean);
-
-        
     }                                                            
 
 
-
-
-
     public static void main(String[] args) {
-
         final ImageJ ij = net.imagej.Main.launch(args);
 //        ij.launch(args);
         Img image = ij.op().create().img(new Long[]{100l, 80l});
         ij.op().image().equation(image, "p[0]+p[1]");
+
         //problem is that 'image' is a raw imglib2 type, not an ij2 dataset. so it gets auto-wrapped, but with a null name.
         // we should also fix things so that when autowrapping happens, name is not null? not sure...
         ImgPlus imgPlus = new ImgPlus((Img) image);
         imgPlus.setName("my awesome image");
         Dataset dataset = new DefaultDataset(ij.context(), imgPlus);
-//        ij.display().createDisplay(image);
         ij.ui().show(dataset);
         ij.command().run(ImageMean.class, true);
     }
-
-
 }
